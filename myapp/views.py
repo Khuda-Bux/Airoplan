1f7fa0b4e65457c80f8ba02c41ef36a9a54dcea9
Khuda-Bux <Khudabuxhalepoto123@gmail.com>
2023-01-28 16:59:19 +0500

    now code properly work

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic import CreateView,TemplateView
 from .models import plan
 from . forms import Myplan
-# Create your views here.
 class regi(CreateView):
     form_class = Myplan
     template_name= 'myapp/home.html'
